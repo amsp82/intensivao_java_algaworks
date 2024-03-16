9712c6bc15c5ef7839c28e796e250ad4ef0a8272
Adelmo Menezes - Debian <adelmo.msp@gmail.com>
2024-03-15 23:42:25 -0300

    Aula 08: Upcasting e poliformismo (warm up)

--- banco/src/com/adeterc/banco/app/Main.java
+++ banco/src/com/adeterc/banco/app/Main.java
@@ -1,5 +1,6 @@
 package com.adeterc.banco.app;
 
+import com.adeterc.banco.modelo.CaixaEletronico;
 import com.adeterc.banco.modelo.ContaEspecial;
 import com.adeterc.banco.modelo.ContaInvestimento;
 import com.adeterc.banco.modelo.Pessoa;
@@ -14,10 +15,14 @@ public class Main {
         Pessoa titular2 = new Pessoa();
         titular2.setNome("Maria Abadia");
         titular2.setDocumento("12332112311");
+        
+        CaixaEletronico caixaEletronico = new CaixaEletronico();
 
         ContaInvestimento minhaConta = new ContaInvestimento(titular1, 123, 987);
         minhaConta.depositar(15_000.00);
 
+        // Conta conta = (Conta) minhaConta; // Upcasting
+        
         ContaEspecial suaConta = new ContaEspecial(titular2, 222, 333, 1_000.00);
         suaConta.depositar(15_000.00);
         suaConta.sacar(15_500.00);
@@ -26,10 +31,16 @@ public class Main {
         minhaConta.sacar(1_000.00, 10.00);
         minhaConta.creditarRendimentos(0.8);
         
+        caixaEletronico.imprimirSaldo(minhaConta);
+        System.out.println("-");
+        caixaEletronico.imprimirSaldo(suaConta);
+        
+        /*
         System.out.println("Nome Titular: " + minhaConta.getTitular().getNome());
         System.out.println("Saldo: " + minhaConta.getSaldo());
 
         System.out.println("Nome Titular: " + suaConta.getTitular().getNome());
         System.out.println("Saldo: " + suaConta.getSaldo());
+       */
     }
 }
